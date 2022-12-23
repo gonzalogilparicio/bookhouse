b2049253f9a5a7a7ab1129462be0d9d073388c34
Gonzalo Gil Paricio <gonzalo.gilparicio@gmail.com>
2022-12-23 16:33:53 -0300

    add CartWidget component

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import './CartWidget.css'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src='./assets/img/cart.svg' />
+            <span>0</span>
+        </div>
+    )
+}
+
+export default CartWidget
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
+import CartWidget from '../CartWidget/CartWidget';
+import logo from '../../assets/img/logo.png';
 import './Navbar.css'
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-light">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="#"><img src={logo} /></a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon" />
       </button>
@@ -31,10 +33,7 @@ const Navbar = () => {
             <a className="nav-link disabled">Disabled</a>
           </li>
         </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <CartWidget />
       </div>
     </div>
   </nav>
