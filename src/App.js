6e56fc16ec1876c8c8808c27e3e9372fad669584
Gonzalo Gil Paricio <gonzalo.gilparicio@gmail.com>
2023-01-11 14:52:13 -0300

    update ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <header className="App-header">
-        <Navbar />        
-      </header>
-      <body>
-        <main>
-          <ItemListContainer  greeting='Prueba de greeting' color='grey'/>
-        </main>
-      </body>
+      <BrowserRouter>      
+        <Navbar /> 
+        <ItemListContainer  greeting='Prueba de greeting' color='grey'/>        
       </BrowserRouter>
     </div>
   );
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,15 +1,38 @@
 import './ItemListContainer.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getProducts } from '../../asyncMock'
-import { useEffect } from 'react'
 
 const ItemListContainer = ({ greeting, color }) => {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    useEffect
+    useEffect (() => {
+        setLoading(true);
+        getProducts().then(productsFromApi => {
+            setProducts(productsFromApi)
+        }).catch(error => {
+            setError(true)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }, [])
+    console.log(products);
 
+    if(loading) {
+        return <h1>Cargando...</h1>;
+    }
+
+    if(error) {
+        return <h1>Hubo un error al conectarse con la base de datos.</h1>;
+    }
+  
     return (
         <div>
             <h1 style={{color}}>{greeting}</h1>
+            <ul>
+                {products.map(prod => <li>{prod.title}</li>)}
+            </ul>
         </div>
     )
 }
