1ca95bb4f1c91b328fac9978ee1a95fca1cd9786
Gonzalo Gil Paricio <gonzalo.gilparicio@gmail.com>
2023-02-10 04:16:58 -0300

    cart code modified

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -1,9 +1,20 @@
 import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
 import CartList from "../CartList/CartList"
+import { Link } from "react-router-dom"
 
 const CartContainer = () => {
     const { cart } = useContext(CartContext)
+
+    if (cart.length === 0) {
+        return (
+            <div>
+                <h4 className="H4" style={{ marginTop: '80px', fontSize: '30px' }}>No hay productos en el carrito</h4>
+                <Link to='/'><p style={{ marginTop: '40px', color: 'black', textDecoration: 'none' }} >Volver al inicio</p></Link>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h4 style={{ color: "black", marginTop: "40px", fontSize: '40px' }}>Finalizá tu compra</h4>
